272ed48abbffa7fb1f331e54652eb0d902de8331
Bruno Dias <brunoald@gmail.com>
2015-11-19 00:46:52 -0200

    Refatoração do método getWorkList

--- produto/código/geraBurndown/src/br/com/tcc/model/BurnUp.java
+++ produto/código/geraBurndown/src/br/com/tcc/model/BurnUp.java
@@ -5,10 +5,9 @@ import java.util.Hashtable;
 import br.com.tcc.util.DataUtil;
 
 public class BurnUp {
-	
+
 	Sprint sprint;
 	Hashtable<Integer, Integer> scope = new Hashtable<Integer, Integer>();
-
 
 	public void setSprint(Sprint sprint) {
 		this.sprint = sprint;
@@ -27,9 +26,9 @@ public class BurnUp {
 		for (int j = 0; j < i; j++) {
 			try {
 				int value = scope.get(j);
-				list[j] = value;				
+				list[j] = value;
 			} catch (Exception e) {
-				list[j] = list[j-1];
+				list[j] = list[j - 1];
 			}
 		}
 		return list;
@@ -37,22 +36,19 @@ public class BurnUp {
 
 	public double[] getWorkList() {
 		Hashtable<String, Double> workTimeByDay = new Hashtable<String, Double>();
-		for (ItemHistorico item : sprint.getItensHistorico()) {
-			workTimeByDay.put(DataUtil.dateToString(item.getData()), 0.0);
-		}
-		for (ItemHistorico item : sprint.getItensHistorico()) {
-			double currentValue = workTimeByDay.get(DataUtil.dateToString(item.getData())); 
-			workTimeByDay.put(DataUtil.dateToString(item.getData()), currentValue + item.getTempoGasto());
-		}
+		int i = 0, soma = 0;
 		int size = sprint.getItensHistorico().size();
 		double[] result = new double[size];
-		int i = 0;
-		int soma = 0;
 		for (ItemHistorico item : sprint.getItensHistorico()) {
-			double value = workTimeByDay.get(DataUtil.dateToString(item.getData()));
-			soma+= value;
+			String key = DataUtil.dateToString(item.getData());
+			double currentValue = 0.0;
+			if (workTimeByDay.containsKey(key))
+				currentValue = workTimeByDay.get(key);
+			workTimeByDay.put(key, currentValue + item.getTempoGasto());
+			double value = workTimeByDay.get(key);
+			soma += value;
 			result[i] = soma;
-			i++;	
+			i++;
 		}
 		return result;
 	}
