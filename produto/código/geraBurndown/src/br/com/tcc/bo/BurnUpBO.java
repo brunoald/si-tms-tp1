ca875c824c32cd220c7d6e8bb681c6f8429f9767
Bruno Dias <brunoald@gmail.com>
2015-11-19 01:03:04 -0200

    Cria o teste para classe BurnUpBO.

---
+++ produto/código/geraBurndown/src/br/com/tcc/bo/BurnUpBO.java
@@ -0,0 +1,14 @@
+package br.com.tcc.bo;
+
+import br.com.tcc.model.BurnUp;
+
+public class BurnUpBO {
+
+	public void setBurnUp(BurnUp burnUp) {
+	}
+
+	public Object getBurnUp() {
+		return null;
+	}
+
+}
---
+++ produto/código/geraBurndown/src/br/com/tcc/tests/BurnUpBOTest.java
@@ -0,0 +1,19 @@
+package br.com.tcc.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.tcc.bo.BurnUpBO;
+import br.com.tcc.model.BurnUp;
+
+public class BurnUpBOTest {
+
+	@Test
+	public void test() {
+		BurnUpBO burnUpBO = new BurnUpBO();
+		BurnUp burnUp = new BurnUp();
+		burnUpBO.setBurnUp(burnUp);
+		assertEquals(burnUpBO.getBurnUp(), burnUp);
+	}
+}
