159be17ca2e15d2bd051069eff406a52d9e63608
Bruno Dias <brunoald@gmail.com>
2015-11-19 00:04:47 -0200

    Cria um teste básico e faz compilar criando a classe BurnUp.

--- produto/código/geraBurndown/src/br/com/tcc/tests/BurnUpTest.java
+++ produto/código/geraBurndown/src/br/com/tcc/tests/BurnUpTest.java
@@ -1,4 +1,13 @@
 package br.com.tcc.tests;
 
+import org.junit.Test;
+
+import br.com.tcc.model.BurnUp;
+
 public class BurnUpTest {
+	
+	@Test
+	public void test() {
+		BurnUp burnup = new BurnUp();
+	}
 }
