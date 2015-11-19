6ba42c8b475d3a4e5ccc46be26a7bad506d69d75
Bruno Dias <brunoald@gmail.com>
2015-11-19 00:07:03 -0200

    Cria um teste para o atributo sprint e faz compilar.

--- produto/código/geraBurndown/src/br/com/tcc/model/BurnUp.java
+++ produto/código/geraBurndown/src/br/com/tcc/model/BurnUp.java
@@ -2,4 +2,10 @@ package br.com.tcc.model;
 
 public class BurnUp {
 
+	public void setSprint(Sprint sprint) {	
+	}
+
+	public Object getSprint() {
+		return null;
+	}
 }
--- produto/código/geraBurndown/src/br/com/tcc/tests/BurnUpTest.java
+++ produto/código/geraBurndown/src/br/com/tcc/tests/BurnUpTest.java
@@ -1,13 +1,19 @@
 package br.com.tcc.tests;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import br.com.tcc.model.BurnUp;
+import br.com.tcc.model.Sprint;
 
 public class BurnUpTest {
 	
 	@Test
-	public void test() {
+	public void testAttributeSprint() {
 		BurnUp burnup = new BurnUp();
+		Sprint sprint = new Sprint();
+		burnup.setSprint(sprint);
+		assertEquals(burnup.getSprint(), sprint);
 	}
 }
